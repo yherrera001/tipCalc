d355e8824c855564eb04d7cf56c4931f077c20f6
yherrera001 <yherrera001@my.wilmu.edu>
2020-10-19 12:32:49 -0400

    first commit to heroku

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core'; 
+import { ViewChild } from '@angular/core'; //importing so that I can refer back to it on the @
+import { ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild("price") priceChild: ElementRef; //Here I am manipulating html. I am grabbing the price element and 
+  @ViewChild("tipPecentage") tipPecentageChild: ElementRef; //priceChild, tipPercentageChild, and peopleChild are simply variables i use in the
+  //method and in the quotes it is grabbing from the html input element
+  @ViewChild("people") peopleChild: ElementRef; //IN OTHER WORDS I am using ViewChild and Template Reference Variables
+  title = 'tipCalc';
+
+  tipTotalPerPerson = 0;
+  tipTotal = 0;
+
+  calculate(price: number, tipPecentage: number, people: number){
+    this.tipTotalPerPerson = ((price * (tipPecentage / 100)) / people) //my function
+    this.tipTotal = this.tipTotalPerPerson * people
+    console.log(this.tipTotalPerPerson) 
+  }
+  onReset(){ //My method and I will be doing dependency injection
+    this.priceChild.nativeElement.value = ''; //Here I can put whatever I want in the single quote to make it reset and then get a different placeholder
+    this.tipPecentageChild.nativeElement.value = ''; //The native element means it is referring back to the htl and the .value means it is inserting the value that the elements in the html have
+    this.peopleChild.nativeElement.value = '';
+  }
+}
